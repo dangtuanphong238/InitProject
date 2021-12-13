9a726b2cd42eafcb7756e6539064b011cf3c9a8c
Đặng Tuấn Phong <Phongstork@gmail.com>
2021-12-13 07:41:03 +0700

    init component

---
+++ src/theme/colors.js
@@ -0,0 +1,17 @@
+export default {
+    secondary: '#4ecdc4',
+    primary: '#d16d72',
+    black: '#000',
+    white: '#fff',
+    medium: '#655858',
+    light: '#f8f4f4',
+    danger: '#ff5252',
+    dark: '#800000',
+    pink: '#FF7979',
+    brown: '#AD2727',
+    red: '#cc0000',
+    purple: '#803C6D',
+    yellow: '#FFD83F',
+    lightPink: '#fcf2f2',
+    grey: '#afb3b0',
+  };
